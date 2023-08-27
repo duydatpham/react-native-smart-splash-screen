6aac3cd701dca34a747aa727863cebe0d1f77934
duydatpham <duydatpham@gmail.com>
2023-08-27 21:33:21 +0700

    #update npm

--- SplashScreen.js
+++ SplashScreen.js
@@ -1,8 +1,8 @@
 /*
  * A smart splash screen for react-native apps
- * https://github.com/react-native-component/react-native-smart-splash-screen/
+ * https://github.com/duydatpham/react-native-smart-splash-screen/
  * Released under the MIT license
- * Copyright (c) 2016 react-native-component <moonsunfall@aliyun.com>
+ * Copyright (c) 2016 react-native-component <duydatpham@gmail.com>
  */
 
 import {
